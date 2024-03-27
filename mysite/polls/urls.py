722377470917625dbf3b98159b948f65ae82ef99
Racz <77125873+Racz-RD@users.noreply.github.com>
2024-03-27 14:43:39 +0100

    FEAT: Use generic views
    Views `index()`, `detail()` and `results()` represent a common case
    of basic web development: getting data from the DB according to a
    parameter in the URL, loading a template and returning the rendered
    template. For common patterns Django provides a shorcut, called the
    "generic views" system. We use `ListView` to display a list of objects
    and `DetailView` to display a detail page for a particular type of
    object.
    Change `polls/urls/py` and `polls/views.py`, delete/comment old views
    and introduce new views based on Django's generic views.

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -32,13 +32,29 @@ app_name = "polls"
 #especially from within templates. This powerful feature allows you to make 
 #global changes to the URL patterns of your project while only touching a single file.
 
+''' URLPATTERNS LIST:
+    Note that the name of the matched pattern in the path strings of 
+    the second and third patterns has changed from <question_id> to <pk>. 
+    This is necessary because we’ll use the DetailView generic view to 
+    replace our detail() and results() views, and it expects 
+    the primary key value captured from the URL to be called "pk".
+'''
+
 urlpatterns = [
     # ex: /polls/
-    path("", views.index, name="index"),
+    #path("", views.index, name="index"),
+    path("", views.IndexView.as_view(), name="index"),
+
     # ex: /polls/5/
-    path("<int:question_id>/", views.detail, name="detail"),
+    #path("<int:question_id>/", views.detail, name="detail"),
+    
+    path("<int:pk>/", views.DetailView.as_view(), name="detail"),
+    
     # ex: /polls/5/results/
-    path("<int:question_id>/results/", views.results, name="results"),
+    #path("<int:question_id>/results/", views.results, name="results"),
+    path("<int:pk>/results/", views.ResultView.as_view(), name="results"),
+    
     # ex: /polls/vote/
+    #path("<int:question_id>/vote/", views.vote, name="vote"),
     path("<int:question_id>/vote/", views.vote, name="vote"),
 ]
--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,10 +1,12 @@
 from django.db.models import F
+from django.db.models.query import QuerySet
 from django.shortcuts import render, get_object_or_404
 """The render() function takes the request object as its first argument, 
 a template name as its second argument and a dictionary as its optional 
 third argument. It returns an HttpResponse object of the given template rendered with the given co"""
 from django.http import Http404, HttpResponse, HttpResponseRedirect
 from django.urls import reverse
+from django.views import generic
 from django.template import loader
 from .models import Question, Choice
 
@@ -26,13 +28,22 @@ from .models import Question, Choice
     }
     return HttpResponse(template.render(context, request)) """
 
-def index(request):
+""" def index(request):
     latest_question_list = Question.objects.order_by("-pub_date")[:5]
     context = {
         "latest_question_list" : latest_question_list,
     }
-    return render(request, "polls/index.html", context)
+    return render(request, "polls/index.html", context) """
 
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
+
+    def get_queryset(self):
+        """Return the last five published
+        questions."""
+        return Question.objects.order_by("-pub_date")[:5]
+    
 
 
 """ def detail(request, question_id):
@@ -47,8 +58,8 @@ def index(request):
 
     return render(request, "polls/detail.html", { "question" : question}) """
 
-def detail(request, question_id):
-    """
+""" def detail(request, question_id):
+    '''
     The get_object_or_404() function takes a Django model 
     as its first argument and an arbitrary number of keyword arguments,
     which it passes to the get() function of the model’s manager. 
@@ -57,19 +68,27 @@ def detail(request, question_id):
     There’s also a get_list_or_404() function, which works just as 
     get_object_or_404() – except using filter() instead of get(). 
     It raises Http404 if the list is empty.
-    """
+    '''
     question = get_object_or_404(Question, pk=question_id)
-    return render(request, "polls/detail.html", { "question" : question})
+    return render(request, "polls/detail.html", { "question" : question}) """
+
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'polls/detail.html'
 
 
 """ def results(request, question_id):
     response = "You're looking at the results of question %s."
     return HttpResponse(response % question_id) """
 
-def results(request, question_id):
+""" def results(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
     return render(request, 
-                  "polls/results.html", {"question": question})
+                  "polls/results.html", {"question": question}) """
+
+class ResultView(generic.DetailView):
+    model = Question
+    template_name = 'polls/results.html'
 
 
 
