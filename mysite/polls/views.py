baa70ced2561defc7fbeeb29c8c857c5fe4a2feb
Racz <77125873+Racz-RD@users.noreply.github.com>
2024-04-02 10:08:25 +0200

    FEAT: Test a view - examples
    Improve IndexView and test it. Test the DetailView. Use separate
    TestClass for each model or view. We use a separate test method for each
    set of conditions we want to test. Test method names describe their
    function.

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -9,6 +9,8 @@ from django.urls import reverse
 from django.views import generic
 from django.template import loader
 from .models import Question, Choice
+from django.utils import timezone
+
 
 # Create your views here.
 
@@ -35,16 +37,24 @@ from .models import Question, Choice
     }
     return render(request, "polls/index.html", context) """
 
-class IndexView(generic.ListView):
-    template_name = 'polls/index.html'
-    context_object_name = 'latest_question_list'
-
-    def get_queryset(self):
-        """Return the last five published
-        questions."""
-        return Question.objects.order_by("-pub_date")[:5]
     
+class IndexView(generic.ListView):
+    template_name = "polls/index.html"
+    context_object_name = "latest_question_list"
 
+
+    """ def get_queryset(self):
+        '''Return the last five published questions.'''
+        return Question.objects.order_by("-pub_date")[:5] """
+    
+    def get_queryset(self):
+        """
+        Return the last five published questions (not including those set to be
+        published in the future).
+        """
+        return Question.objects.filter(pub_date__lte=timezone.now()).order_by("-pub_date")[
+            :5
+        ]   
 
 """ def detail(request, question_id):
     return HttpResponse("You're looking at question %s." % question_id) """
@@ -75,6 +85,12 @@ class IndexView(generic.ListView):
 class DetailView(generic.DetailView):
     model = Question
     template_name = 'polls/detail.html'
+
+    def get_queryset(self):
+        """
+        Excludes any questions that aren't published yet.
+        """
+        return Question.objects.filter(pub_date__lte=timezone.now())
 
 
 """ def results(request, question_id):
