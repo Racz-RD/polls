93fd91d76d33571625543e5cb22c6ee542fc919e
Racz <77125873+Racz-RD@users.noreply.github.com>
2024-04-03 15:01:59 +0200

    FEAT: Customize the admin form
    Change the admin options for a model. We create a model admin class and
    pass it as the second argument to `admin.site.register()`. Add related
    objects and customize the admin change list. Django offers a tabular way
    of displaying inline related objects. We use `display()` decorator and
    add filter to sidebar. In addition we add search capability that adds
    a search box at the top of the change list.

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -1,6 +1,38 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Question
+from .models import Question, Choice
 
-admin.site.register(Question)
+# Default form representation
+# admin.site.register(Question)
+
+""" class QuestionAdmin(admin.ModelAdmin):
+    fields=['pub_date', 'question_text'] """
+
+# It takes a lot of screen space to display all the fields for entering related Choice objects.  
+""" class ChoiceInline(admin.StackedInline):
+    model = Choice
+    extra = 3 """
+
+# Django offers a tabular way of displaying inline related objects. 
+# With that TabularInline (instead of StackedInline), 
+# the related objects are displayed in a more compact, table-based format. 
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3  
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {'fields': ['question_text']}),
+        ('Date information', {'fields':['pub_date']}),
+    ]
+    inlines = [ChoiceInline]
+    list_display = ['question_text', 'pub_date', 'was_published_recently']
+    list_filter = ['pub_date']
+    search_fields = ['question_text']
+
+# Create a model admin class, then pass it as the second argument to admin.site.register() – 
+# any time you need to change the admin options for a model.
+# This particular change above makes the “Publication date” come before the “Question” field:   
+admin.site.register(Question, QuestionAdmin)
+
